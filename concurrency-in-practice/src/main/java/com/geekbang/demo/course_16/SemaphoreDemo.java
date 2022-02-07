c465e78ceb5e1b22bc8b7ce42e3bf3508b955ff0
yangweiping <yangweiping@meituan.com>
2022-02-07 17:46:45 +0800

    增加demo代码

--- concurrency-in-practice/src/main/java/com/geekbang/demo/course_16/SemaphoreDemo.java
+++ concurrency-in-practice/src/main/java/com/geekbang/demo/course_16/SemaphoreDemo.java
@@ -8,11 +8,50 @@ import java.util.concurrent.Semaphore;
  */
 public class SemaphoreDemo {
 
+    static int count;
+
+    static final Semaphore s = new Semaphore(0);
+
+    static void addOne() {
+        try {
+            s.acquire();
+            count += 1;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // 只允许一个线程, 其他线程被阻塞
+//            s.release();
+        }
+    }
+
+
     public static void main(String[] args) {
 
-        Semaphore semaphore = new Semaphore(1);
+        MutliThread mutliThread = new MutliThread();
+
+        Thread t1=new Thread(mutliThread,"Window 1");
+        Thread t2=new Thread(mutliThread,"Window 2");
+        t1.start();
+        t2.start();
 
 
+    }
 
+
+}
+
+class MutliThread implements Runnable {
+
+    @Override
+    public void run() {
+        SemaphoreDemo.addOne();
+
+        System.out.println("----" + Thread.currentThread().getName());
+
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
