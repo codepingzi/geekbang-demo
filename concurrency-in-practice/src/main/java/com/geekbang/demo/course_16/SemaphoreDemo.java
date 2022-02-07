e6c873589263a74a289789ef611b8f9afebf3766
yangweiping <yangweiping@meituan.com>
2022-02-07 17:27:46 +0800

    first commit

---
+++ concurrency-in-practice/src/main/java/com/geekbang/demo/course_16/SemaphoreDemo.java
@@ -0,0 +1,18 @@
+package com.geekbang.demo.course_16;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author yangweiping
+ * @date 2022/2/7 下午5:17
+ */
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+
+        Semaphore semaphore = new Semaphore(1);
+
+
+
+    }
+}
