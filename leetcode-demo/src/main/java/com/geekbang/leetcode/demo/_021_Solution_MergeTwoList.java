998ff15d95ee5473453284dcd634209d3a1e1af3
yangweiping <yangweiping@meituan.com>
2022-02-24 14:10:07 +0800

    增加迭代循环方式

--- leetcode-demo/src/main/java/com/geekbang/leetcode/demo/_021_Solution_MergeTwoList.java
+++ leetcode-demo/src/main/java/com/geekbang/leetcode/demo/_021_Solution_MergeTwoList.java
@@ -9,24 +9,53 @@ package com.geekbang.leetcode.demo;
 public class _021_Solution_MergeTwoList {
 
     public static void main(String[] args) {
+        ListNode listNode14 = new ListNode(4);
+        ListNode listNode12 = new ListNode(2, listNode14);
+        ListNode listNode11 = new ListNode(1, listNode12);
 
+        ListNode listNode24 = new ListNode(4);
+        ListNode listNode23 = new ListNode(3, listNode24);
+        ListNode listNode21 = new ListNode(1, listNode23);
+
+        _021_Solution_MergeTwoList solution = new _021_Solution_MergeTwoList();
+        ListNode listNode = solution.mergeTwoLists_01(listNode11, listNode21);
+        solution.print(listNode);
     }
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null){
-            return list2;
+    private void print(ListNode listNode){
+        while(listNode!=null){
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+    /**
+     * 迭代方式
+     * 1. 设计一个前置节点 preNode 其value=-1,
+     * 2. 比较listNode1 和 listNode的首节点, value小的一个被前置节点preNode.next指向
+     * 3. 然后移动前置节点指针为这个next节点,
+     * 4. 继续比较指针的next节点和另一个listNode的首节点, 直到后面节点为null
+     * 5. 返回第二个节点的listNode
+     * @param listNode1
+     * @param listNode2
+     * @return
+     */
+    public ListNode mergeTwoLists_01(ListNode listNode1, ListNode listNode2) {
+        ListNode preHead = new ListNode(-1);
+        ListNode preNode = preHead;
+        while(listNode1 != null && listNode2 != null){
+            if (listNode1.val <= listNode2.val){
+                preNode.next = listNode1;
+                listNode1 = listNode1.next;
+            }else{
+                preNode.next = listNode2;
+                listNode2 = listNode2.next;
+            }
+            preNode = preNode.next;
         }
 
-        if (list2 == null){
-            return list1;
-        }
-
-        while(list1.next != null){
-            System.out.println(list1.next);
-        }
-
-
-
+        preNode.next = listNode1 != null ? listNode1 : listNode2;
+        return preHead.next;
     }
 }
 
